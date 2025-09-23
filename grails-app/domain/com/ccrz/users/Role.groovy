d2bb6c8586e4e40d4bbe92a51b99a9e0927e1801
Raul Reyes <137012254+RaulReyesB@users.noreply.github.com>
2025-09-22 22:37:16 -0600

    agregando relaciones de los modelos

--- grails-app/domain/com/ccrz/users/Role.groovy
+++ grails-app/domain/com/ccrz/users/Role.groovy
@@ -3,7 +3,13 @@ package com.ccrz.users
 class Role {
     String rol
     String status = "active"
+
+    static hasMany = [userRoles: UserRole]
     static constraints = {
         status inList : ["active", "inactive"]
+    }
+
+    static mapping = {
+        table name:"tbb_Role"
     }
 }
--- grails-app/domain/com/ccrz/users/UserRole.groovy
+++ grails-app/domain/com/ccrz/users/UserRole.groovy
@@ -1,8 +1,16 @@
 package com.ccrz.users
 
 class UserRole {
+    Date created_at
+    Date update_at
+
+    static belongsTo = [user: User, role: Role]
 
   static constraints = {
 
+  }
+  static mapping = {
+    table name:"tbd_User_Role"
+    version false
   }
 }
